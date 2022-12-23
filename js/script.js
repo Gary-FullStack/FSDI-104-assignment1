1e90d9f07f114cc17586d5a469dfa767d4b77a42
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-23 12:11:04 -0500

    another commit
    will I ever get this done?

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-ddd
+
+let petSalon = {
+    name: "Pets R Us",
+    phone: "123-456-7890",
+    address: {
+        location: "123 Street",
+        city: "Blarney-Ville",
+        zip: "12345"
+    }
+
+}
+
+
+
+
+
+function init() { }
+
+window.onload = init;
