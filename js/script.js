6a29f139c53642cf6110fdb47edd0f50e05d9031
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-26 08:21:49 -0500

    another commit
    after holidays ..  where the hell did I leave off?    Gaaa ,,taking a break is bad joo joo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 let petSalon = {
-    name: "Pets R Us",
+    tiTle: "Pets R Us",
     phone: "123-456-7890",
     address: {
         location: "123 Street",
@@ -10,10 +10,45 @@ let petSalon = {
 
 }
 
+let Pets = {
+    naMe: "Jake",
+    type: "dog",
+    breed: "Lab",
+    gender: "male",
+    age: "13",
+    temperament: "calm",
+    demeanor: "friendly"
+};
+
+let Ivy = {
+    type: `dog`,
+    breed: `mix`,
+    gender: `female`,
+    age: `3`,
+    temperament: `hyper`,
+    demeanor: `guarded`
+
+};
+
+let Penny = {
+    type: `dog`,
+    breed: `Australian Cattle`,
+    gender: `female`,
+    age: `10`,
+    temperament: `loving`,
+    demeanor: `beautiful soul`,
+
+};
+
+alert("There are three Dogs registered today.");
+
+console.log();
+
+
 
 
 
 
 function init() { }
 
-window.onload = init;
+window.onload = init; 
